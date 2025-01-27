9f5063591e6c36a401853e9c63dd5907bbbf6989
Sujan-1990 <sujrap@gmail.com>
2025-01-27 12:17:37 +0545

    unnecessary component calls removed

--- src/components/Orders.tsx
+++ src/components/Orders.tsx
@@ -1,4 +1,4 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { IOrder } from "../interfaces/order";
 import OrderItem from "./OrderItem";
